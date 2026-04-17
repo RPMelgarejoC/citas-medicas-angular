db0df6e02fa23e1e60d9b14caae8045715d4f04c
RPMelgarejoC <ronaldo.melgarejo@uab.edu.bo>
2026-04-17 10:02:54 -0400

    Configuración Inicial, routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Pacientes } from './pacientes/pacientes';
+import { Medicos } from './medicos/medicos';
+import { Citas } from './citas/citas';
+
+
+export const routes: Routes = [
+    { path: 'pacientes', component: Pacientes },
+    { path: 'medicos', component: Medicos },
+    { path: 'citas', component: Citas },
+    { path: '', redirectTo: 'pacientes', pathMatch: 'full' }
+
+];
