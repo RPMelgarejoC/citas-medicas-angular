07ec6df9ba2189c25a3fa9f0b5ce62ec87914260
RPMelgarejoC <ronaldo.melgarejo@uab.edu.bo>
2026-04-17 11:04:15 -0400

    CRUD básico de Pacientes

--- src/app/pacientes/pacientes.ts
+++ src/app/pacientes/pacientes.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-pacientes',
-  imports: [],
+  imports: [FormsModule, NgFor],
   templateUrl: './pacientes.html',
   styleUrl: './pacientes.css',
 })
-export class Pacientes {}
+export class Pacientes {
+  pacientes: any[] = [];
+
+  nuevoPaciente = {
+    nombre: '',
+    apellido: '',
+    edad: 0,
+    telefono: '',
+    email: ''
+  };
+
+  agregarPaciente() {
+    const paciente = {
+      id: Date.now(),
+      ...this.nuevoPaciente
+    };
+
+    this.pacientes.push(paciente);
+
+    // limpiar formulario
+    this.nuevoPaciente = {
+      nombre: '',
+      apellido: '',
+      edad: 0,
+      telefono: '',
+      email: ''
+    };
+  }
+
+  eliminarPaciente(id: number) {
+    this.pacientes = this.pacientes.filter(p => p.id !== id);
+  }
+}
